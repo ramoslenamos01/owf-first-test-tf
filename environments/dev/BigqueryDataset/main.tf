4f36a22c0a31caf97371e8e83013b78452eb5522
Othmane Wafi <owafi@frl3348.home>
2022-06-15 15:37:13 +0200

    cloudbuild

---
+++ environments/dev/BigqueryDataset/main.tf
@@ -0,0 +1,27 @@
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+
+
+  
+}
+
+resource "google_bigquery_dataset" "default" {
+  dataset_id                  = "othy"
+  friendly_name               = "othmane"
+  description                 = "This is a test description"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+}
