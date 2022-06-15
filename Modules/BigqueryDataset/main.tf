6e65a769a5d15db4b51d8bc8c00da4386c6c615e
Othmane Wafi <owafi@frl3348.home>
2022-06-15 16:07:30 +0200

    cloudbuild

--- Modules/BigqueryDataset/main.tf
+++ Modules/BigqueryDataset/main.tf
@@ -1,12 +1,5 @@
 
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "3.5.0"
-    }
-  }
-}
+
 
 provider "google" {
 project = "sandbox-owafi"
